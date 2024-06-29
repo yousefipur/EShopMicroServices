7cdc58fd0edfc0d4dee48d9f69e56063c1347dbe
Alireza <yousefipur.a@gmail.com>
2024-06-29 16:54:49 +0330

    ...

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Carter;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.GetProducts
+{
+    //public record GetProductsRequest();
+    public record GetProductsResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProduct")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Get Product")
+            .WithSummary("Get Product"); ;
+        }
+    }
+}
