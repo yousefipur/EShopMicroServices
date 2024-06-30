a5ee3996ba1d487ad8b192db2569772c176ff537
Alireza <yousefipur.a@gmail.com>
2024-07-01 01:02:06 +0330

    ...

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+
+    internal class DeleteProductCommandHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
